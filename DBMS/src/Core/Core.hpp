014878a6521d0d5059336ed7c730c2348978be6e
TrueDim4 <koch.dima2017@yandex.ru>
2020-12-27 18:08:30 +0300

    renamed some files + changed cmakelists

---
+++ DBMS/src/Core/Core.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <iostream>
+#include <thread>
+#include <mutex>
+
+#include "../EncoderAndDecoder/Encryption.hpp"
+#include "../FileHadnler/FileWorker.hpp"
+#include "../Logger/Logger.hpp"
+#include "../MenuHadnler/Menu.hpp"
+#include "../RequestHandler/Parser.hpp"
+
